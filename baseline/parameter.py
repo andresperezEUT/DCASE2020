ca7b9080a8a76f1dfc0e780b22db4c8a5e240f98
Andres Pérez López <andres.perez@eurecat.org>
2020-05-30 21:44:22 +0200

    some quick results

--- baseline/parameter.py
+++ baseline/parameter.py
@@ -183,6 +183,22 @@ def get_params(argv='1'):
 
         params['preset_descriptor'] = 'loc:dereverb_filter; beam:beam; cls:random_forest'
 
+    if argv == 'mi_primerito_dia_postfilter':
+        # localization_detection
+        params['ld_method'] = 'ld_basic_dereverb_filter'
+        params['ld_method_args'] = [0.3, 5, 4]  # [diff_th, L, event_minimum_length]
+        # beamforming
+        params['beamforming_mode'] = 'beam'
+        # classification
+        params['class_method'] = 'event_class_prediction'
+        params['class_method_args'] = ['event_class_rf']
+        # postprocessing
+        params['event_filter_activation']= True
+        params['event_filter_method'] = 'event_filter_v1'
+        params['event_filter_method_args']=[5,10] #[frames threshold, frames_threshold_fp_8]
+
+        params['preset_descriptor'] = 'loc:dereverb_filter; beam:beam; cls:random_forest; postfilter:v1'
+
     if argv == 'mi_primerito_dia_xgb':
         # localization_detection
         params['ld_method'] = 'ld_basic_dereverb_filter'
