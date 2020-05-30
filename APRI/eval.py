44d2667213bcf978d22dfe23ceec137635d64f58
Andres Pérez López <andres.perez@eurecat.org>
2020-05-30 21:41:31 +0200

    added optional plot to eval.py, added file names to plots

--- APRI/eval.py
+++ APRI/eval.py
@@ -7,11 +7,14 @@ evaluate precomputed output files
 from baseline import parameter
 import os
 from APRI.compute_metrics import compute_metrics
+from APRI.utils import plot_results
 
 
 # %% PARAMS
 
-preset = 'mi_primerito_dia'
+plot = False
+
+preset = 'mi_primerito_dia_postfilter_Q!'
 params = parameter.get_params(preset)
 gt_folder = os.path.join(params['dataset_dir'], 'metadata_dev')  # path to annotations
 this_file_path = os.path.dirname(os.path.abspath(__file__))
@@ -21,3 +24,12 @@ result_folder_path = os.path.join(this_file_path, params['results_dir'], preset)
 # %% RUN
 
 compute_metrics(gt_folder, result_folder_path, params)
+
+
+# %% PLOT
+
+# Achtung! will plot *all* metadata result files
+res_files = [f for f in os.listdir(result_folder_path) if f != '.DS_Store']
+if plot:
+    for res_file in res_files:
+        plot_results(os.path.join(result_folder_path,res_file), params)
