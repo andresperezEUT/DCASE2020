fb253aded91e25342e11f33df6b5e7dd7d31dd38
ribanez <ribanez@stratio.com>
2020-06-01 08:56:34 +0200

    audio features extractor changed. Ols models doesnt work anymore

--- APRI/event_class_prediction.py
+++ APRI/event_class_prediction.py
@@ -16,50 +16,10 @@ import essentia.standard as ess
 import random
 from essentia.standard import MusicExtractor as ms
 from APRI.utils import get_class_name_dict
+from APRI.get_audio_features import *
 import sys
 
 ### FUNCIONES PARA PREDICCIÓN DE EVENT_CLASS ###
-
-def get_feature_list():
-    return [
-         'lowlevel.barkbands.dmean',
-         'lowlevel.barkbands.mean',
-         'lowlevel.barkbands.var',
-         'lowlevel.erbbands.dmean',
-         'lowlevel.erbbands.mean',
-         'lowlevel.erbbands.var',
-         'lowlevel.gfcc.mean',
-         'lowlevel.melbands.dmean',
-         'lowlevel.melbands.mean',
-         'lowlevel.melbands.var',
-         'lowlevel.mfcc.mean',
-         'lowlevel.spectral_contrast_coeffs.dmean',
-         'lowlevel.spectral_contrast_coeffs.mean',
-         'lowlevel.spectral_contrast_coeffs.var',
-         'lowlevel.spectral_contrast_valleys.dmean',
-         'lowlevel.spectral_contrast_valleys.mean',
-         'lowlevel.spectral_contrast_valleys.var',
-         'rhythm.beats_loudness_band_ratio.dmean',
-         'rhythm.beats_loudness_band_ratio.mean',
-         'rhythm.beats_loudness_band_ratio.var',
-         'tonal.hpcp.dmean',
-         'tonal.hpcp.mean',
-         'tonal.hpcp.var',
-         'tonal.chords_histogram',
-         'tonal.thpcp',
-         'lowlevel.pitch_salience.dmean',
-         'lowlevel.pitch_salience.mean',
-         'lowlevel.pitch_salience.var',
-         'lowlevel.silence_rate_20dB.dmean',
-         'lowlevel.silence_rate_20dB.mean',
-         'lowlevel.silence_rate_20dB.var',
-         'lowlevel.silence_rate_30dB.dmean',
-         'lowlevel.silence_rate_30dB.mean',
-         'lowlevel.silence_rate_30dB.var',
-         'lowlevel.silence_rate_60dB.dmean',
-         'lowlevel.silence_rate_60dB.mean',
-         'lowlevel.silence_rate_60dB.var'
-    ]
 
 def get_key(val):
     classes=get_class_name_dict()
@@ -68,46 +28,21 @@ def get_key(val):
              return key
 
 def get_features_music_extractor(audio):
-    #TODO: set verbose parameter to false
-    #TODO: add parameter 'analysisSampleRate=24000' in order to speed up process
-    # In MusicExtractor.compute: Parameter sampleRate = 24000 is not within specified range: {32000,44100,48000}
-    try:
-        features, features_frames = ms(
-                                                  analysisSampleRate=44100,
-                                                  chromaprintCompute=True,
-                                                  lowlevelFrameSize=4096,
-                                                  lowlevelHopSize=2048,
-                                                  tonalFrameSize=4096,
-                                                  tonalHopSize=2048,
-                                                  rhythmStats=["mean", "var", "dmean"],
-                                                  lowlevelStats=["mean", "var", "dmean"],
-                                                  )(audio)
-        feature_list = get_feature_list()
-        audio_features = []
-        for feature in feature_list:
-            x = features[feature]
-            if type(x) is float:
-                x = np.array(x)
-                y = [x]
-            else:
-                y = x.tolist()
-            audio_features = audio_features + y
-        audio_features = np.array(audio_features)
-        audio_features.tolist()
-    except:
-        audio_features=[]
-        print('MusicExtractor returns an error. Setting random class...')
+    options = dict()
+    options['sampleRate'] = 24000
+    options['frameSize'] = 2048
+    options['hopSize'] = 1024
+    options['skipSilence'] = True
+    audio_features, column_labels = compute_audio_features_from_audio(audio, options)
     return audio_features
 
 def get_event_class_model(model_name):
-    #TODO: avoid load the model for each audio file
     model_input_path = os.path.dirname(os.path.realpath(__file__)) + '/models/'+model_name+'/model.joblib'
     model = joblib.load(model_input_path)
     return model
 
 
 def event_class_prediction(audio,model_name):
-
     variables=get_features_music_extractor(audio)
     model=get_event_class_model(model_name)
     if len(variables)==0:
