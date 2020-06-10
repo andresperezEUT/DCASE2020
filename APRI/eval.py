d4206f12001c4a4c9ed2f4797b75ae5a0595220b
Andres Pérez López <andres.perez@eurecat.org>
2020-06-10 18:17:01 +0200

    plot eval on selected files

--- APRI/eval.py
+++ APRI/eval.py
@@ -12,9 +12,9 @@ from APRI.utils import plot_results
 
 # %% PARAMS
 
-plot = False
+plot = True
 
-preset = 'mi_primerito_dia_postfilter_Q!'
+preset = 'particle'
 params = parameter.get_params(preset)
 gt_folder = os.path.join(params['dataset_dir'], 'metadata_dev')  # path to annotations
 this_file_path = os.path.dirname(os.path.abspath(__file__))
@@ -30,6 +30,20 @@ compute_metrics(gt_folder, result_folder_path, params)
 
 # Achtung! will plot *all* metadata result files
 res_files = [f for f in os.listdir(result_folder_path) if f != '.DS_Store']
+res_files = ['fold1_room1_mix007_ov1.csv',
+             'fold2_room1_mix007_ov1.csv',
+             'fold3_room1_mix007_ov1.csv',
+             'fold4_room1_mix007_ov1.csv',
+             'fold5_room1_mix007_ov1.csv',
+             'fold6_room1_mix007_ov1.csv',
+             'fold2_room1_mix008_ov1.csv',
+             'fold2_room1_mix009_ov1.csv',
+             'fold2_room1_mix010_ov1.csv',
+             'fold2_room1_mix047_ov2.csv',
+             'fold2_room1_mix048_ov2.csv',
+             'fold2_room1_mix049_ov2.csv'
+             ]
+
 if plot:
     for res_file in res_files:
         plot_results(os.path.join(result_folder_path,res_file), params)
